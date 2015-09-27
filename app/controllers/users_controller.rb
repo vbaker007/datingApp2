fa4643a7b0e28667e6ed94bb4ed506103f710915
vbaker007 <vbaker007@hotmail.com>
2015-09-27 15:03:16 -0600

    update users_controller.rb

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,4 +10,18 @@ class UsersController < ApplicationController
   def new
     @user = User.new
   end
+
+  def create
+    @user = User.new(user_params)
+
+    if @user.save
+      redirect_to users_path
+    else
+      render :new
+    end
+  end
+
+  def user_params
+    params.require(:user).permit(:name, :gender, :age, :occupation, :heterosexual, :age_age_pref, :zipcode, :hobbies)
+  end
 end
