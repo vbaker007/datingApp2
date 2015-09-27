a9f78a53add24293399fa8923b4b623c8ddc87ea
vbaker007 <vbaker007@hotmail.com>
2015-09-27 01:52:35 -0600

    most recent updates

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,23 @@
 Rails.application.routes.draw do
+  get 'users/index'
+
+  get 'users/show'
+
+  get 'users/new'
+
+  resoures :users
+
+
+  root: 'users#index'
+
+
+
+
+
+
+
+
+end
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
